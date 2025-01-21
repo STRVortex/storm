6222769585eb3931dfa035864bdc40eb25954038
STRVortex <vortex8934@gmail.com>
2025-01-21 20:03:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kxunal/Lv2",
+    "https://github.com/STRVortex/storm",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "mainr")
 GIT_TOKEN = getenv(
