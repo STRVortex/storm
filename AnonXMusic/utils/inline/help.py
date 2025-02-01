439c9d1fadc5173a322affe1ad8500f90977df59
STRVortex <vortex8934@gmail.com>
2025-02-01 12:50:13 +0530

    Update help.py

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -21,6 +21,10 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_14"],
                     callback_data="help_callback hb15",
                 ),
+                InlineKeyboardButton(
+                    text=_["H_B_16"],
+                    callback_data="help_callback hb16",
+                ),
             ],
             [
                 InlineKeyboardButton(
@@ -82,6 +86,10 @@ def help_pannel(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(
                     text=_["H_B_13"],
                     callback_data="help_callback hb13",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_15"],
+                    callback_data="help_callback hb15",
                 ),
             ],
             mark,
