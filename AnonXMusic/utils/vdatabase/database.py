525d6906450e6911df5ce15c3671c98bf7bef803
STRVortex <vortex8934@gmail.com>
2025-02-01 13:05:00 +0530

    Create database.py

---
+++ AnonXMusic/utils/vdatabase/database.py
@@ -0,0 +1,81 @@
+import random
+from typing import Dict, List, Union
+
+from AnonXMusic import userbot
+from AnonXMusic.core.mongo import mongodb, pymongodb
+
+authdb = mongodb.adminauth
+authuserdb = mongodb.authuser
+autoenddb = mongodb.autoend
+assdb = mongodb.assistants
+blacklist_chatdb = mongodb.blacklistChat
+blockeddb = mongodb.blockedusers
+chatsdb = mongodb.chats
+channeldb = mongodb.cplaymode
+countdb = mongodb.upcount
+gbansdb = mongodb.gban
+langdb = mongodb.language
+onoffdb = mongodb.onoffper
+playmodedb = mongodb.playmode
+playtypedb = mongodb.playtypedb
+skipdb = mongodb.skipmode
+sudoersdb = mongodb.sudoers
+usersdb = mongodb.tgusersdb
+privatedb = mongodb.privatechats
+suggdb = mongodb.suggestion
+cleandb = mongodb.cleanmode
+queriesdb = mongodb.queries
+userdb = mongodb.userstats
+videodb = mongodb.vipvideocalls
+chatsdbc = mongodb.chatsc  # for clone
+usersdbc = mongodb.tgusersdbc  # for clone
+
+# Shifting to memory [mongo sucks often]
+active = []
+activevideo = []
+assistantdict = {}
+autoend = {}
+count = {}
+channelconnect = {}
+langm = {}
+loop = {}
+maintenance = []
+nonadmin = {}
+pause = {}
+playmode = {}
+playtype = {}
+skipmode = {}
+privatechats = {}
+cleanmode = []
+suggestion = {}
+mute = {}
+audio = {}
+video = {}
+
+# Total Queries on bot
+
+async def get_assistant(chat_id: int) -> str:
+    from AnonXMusic.core.userbot import assistants
+
+    assistant = assistantdict.get(chat_id)
+    if not assistant:
+        dbassistant = await assdb.find_one({"chat_id": chat_id})
+        if not dbassistant:
+            userbot = await set_assistant(chat_id)
+            return userbot
+        else:
+            got_assis = dbassistant["assistant"]
+            if got_assis in assistants:
+                assistantdict[chat_id] = got_assis
+                userbot = await get_client(got_assis)
+                return userbot
+            else:
+                userbot = await set_assistant(chat_id)
+                return userbot
+    else:
+        if assistant in assistants:
+            userbot = await get_client(assistant)
+            return userbot
+        else:
+            userbot = await set_assistant(chat_id)
+            return userbot
